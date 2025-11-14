d178606e37c7b62e8474776bcb39369ab753e0ea
Bark Maxim <maximalen1999@gmail.com>
2025-11-14 12:11:00 +0300

    MP12: Добавил PalindromeChecker

---
+++ src/main/java/ru/mentee/power/PalindromeChecker.java
@@ -0,0 +1,19 @@
+package ru.mentee.power;
+
+import java.util.Scanner;
+
+public class PalindromeChecker {
+
+    public static boolean isPalindrome(String str) {
+        str = str.replaceAll("\\s+", "").toLowerCase();
+        int left = 0, right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
