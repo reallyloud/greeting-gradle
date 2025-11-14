715d43eb1ffe77a12c308ef7cc287c2656c5c641
Bark Maxim <maximalen1999@gmail.com>
2025-11-14 09:54:55 +0300

    MP11.2: Вернул тесты

--- src/test/java/ru/mentee/power/GreetingTest.java
+++ src/test/java/ru/mentee/power/GreetingTest.java
@@ -1,4 +1,17 @@
+package ru.mentee.power;
+
+import org.junit.jupiter.api.Test;
+import ru.mentee.power.Greeting;
 import static org.junit.jupiter.api.Assertions.*;
+
 class GreetingTest {
-  
+
+    @Test
+    void shouldGreeting() {
+        Greeting greeting = new Greeting();
+        String result = greeting.greeting();
+        assertEquals(result, "Hello MP-11 done");
+    }
+
+
 }
